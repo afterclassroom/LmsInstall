005dee7cdebb17dcfc0391fc31e871f0e24fc63f
DannyTran <dungtqa@gmail.com>
2015-04-04 10:46:20 +0700

    Init

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Install LMS script
+
+sudo apt-get update
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' docker|grep -c "ok installed")
+echo Checking for docker: $PKG_OK
+if [ "" == "$PKG_OK" ]; then
+  echo "No docker. Install docker."
+  wget -qO- https://get.docker.com/ | sh
+fi
+
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' fig|grep -c "ok installed")
+echo Checking for fig: $PKG_OK
+if [ "" == "$PKG_OK" ]; then
+  echo "No fig. Install fig."
+  curl -L https://github.com/docker/fig/releases/download/1.0.1/fig-`uname -s`-`uname -m` > /usr/local/bin/fig; chmod +x /usr/local/bin/fig
+fi
+
+fig build  --no-cache
+
+fig up -d
+
+fig run web rake db:migrate bower:install
+
